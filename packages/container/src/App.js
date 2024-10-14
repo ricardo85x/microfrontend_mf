3a9ed44c80fe7e89b2267dd702c9a43910b5022f
Ricardo <ricardo85x@gmail.com>
2024-10-14 15:10:42 -0300

    feat: initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MarketingApp } from './components/MarketingApp'
+
+
+
+export default () => {
+
+    return <div>
+        <h2>Hello Contaner</h2>
+        <hr />
+        <MarketingApp />
+    </div>
+        
+    
+}
